52d19891f942ff0deaff1933d17e36e6951281a0
Jmnote <opcore@gmail.com>
2022-10-07 01:02:40 +0900

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
