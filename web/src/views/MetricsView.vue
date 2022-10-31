ea45b490433ec229edacb28f426d998dd5dd9d1c
jmnote <jmnote@gmail.com>
2022-10-31 07:30:56 +0000

    .

--- web/src/views/MetricsView.vue
+++ web/src/views/MetricsView.vue
@@ -5,10 +5,8 @@ import RunButton from "@/components/RunButton.vue"
 </script>
 
 <template>
-  <header
-    class="fixed right-0 w-full bg-white border-b border-common shadow z-30 p-2 pl-52"
-    :class="{ 'is-loading': loading }"
-  >
+  <header class="fixed right-0 w-full bg-white border-b border-common shadow z-30 p-2 pl-52"
+    :class="{ 'is-loading': loading }">
     <div class="flex items-center flex-row">
       <div>
         <i class="mdi mdi-18px mdi-numeric"></i> Metrics
@@ -18,12 +16,7 @@ import RunButton from "@/components/RunButton.vue"
           <TimeRangePicker @updateTimeRange="updateTimeRange" />
         </span>
         <span class="ml-2">
-          <RunButton
-            btnText="Run query"
-            :disabled="busy"
-            @execute="execute"
-            @changeInterval="changeInterval"
-          />
+          <RunButton btnText="Run query" :disabled="busy" @execute="execute" @changeInterval="changeInterval" />
         </span>
       </div>
     </div>
@@ -32,24 +25,13 @@ import RunButton from "@/components/RunButton.vue"
     <div class="flex-1 py-4 px-4">
       <div class="pb-4">
         <div class="relative w-full">
-          <input
-            type="search"
+          <input type="search"
             class="flex-1 relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            placeholder="Expression"
-            aria-label="Expression"
-            aria-describedby="button-addon3"
-            v-model="expr"
-            @keyup="searchKeyUp"
-          />
-          <ul
-            class="absolute bg-white border max-h-[70vh] overflow-y-auto z-20"
-            v-if="searchMode && expr"
-          >
-            <li
-              class="flex gap-3 hover:bg-gray-200 cursor-pointer"
-              v-for="item in items"
-              @click="expr = item[0]; searchMode = false"
-            >
+            placeholder="Expression" aria-label="Expression" aria-describedby="button-addon3" v-model="expr"
+            @keyup="searchKeyUp" />
+          <ul class="absolute bg-white border max-h-[70vh] overflow-y-auto z-20" v-if="searchMode && expr">
+            <li class="flex gap-3 hover:bg-gray-200 cursor-pointer" v-for="item in items"
+              @click="expr = item[0]; searchMode = false">
               <div class="text-gray-600" v-html="item[2]"></div>
               <div class="flex-auto text-right text-gray-500">{{ item[1][0].type }}</div>
             </li>
@@ -67,44 +49,28 @@ import RunButton from "@/components/RunButton.vue"
 
           <div class="mt-4 py-1 font-bold">
             <span v-if="result && result.length > 0">{{ result.length }} rows</span>
-            <div
-              class="float-right"
-              v-if="cursorTime"
-            >{{ useTimeStore().timestamp2ymdhis(cursorTime) }}</div>
+            <div class="float-right" v-if="cursorTime">{{ useTimeStore().timestamp2ymdhis(cursorTime) }}</div>
           </div>
-          <div
-            class="overflow-x-auto overflow-y-auto margin-l-[5em] max-h-[50vh]"
-            :style="{ width: tableWitdh + 'px' }"
-          >
+          <div class="overflow-x-auto overflow-y-auto margin-l-[5em] max-h-[50vh]"
+            :style="{ width: tableWitdh + 'px' }">
             <table class="whitespace-nowrap border-separate w-full" style="border-spacing:0">
               <tr class="sticky z-10 top-0 border-y bg-slate-200 text-left">
                 <th
                   class="font-normal max-w-[100px] px-2 border border-r-0 text-ellipsis overflow-hidden hover:whitespace-normal hover:min-w-[200px]"
-                  v-for="key in keys"
-                >{{ key }}</th>
-                <th
-                  class="min-w-[120px] sticky top-0 right-0 font-normal border bg-slate-200 text-center"
-                >VALUE</th>
+                  v-for="key in keys">{{ key }}</th>
+                <th class="min-w-[120px] sticky top-0 right-0 font-normal border bg-slate-200 text-center">VALUE</th>
               </tr>
-              <tr v-if="result && result.length>0" v-for="row in result" class="border-b hover:bg-gray-200">
+              <tr v-if="result && result.length > 0" v-for="row in result" class="border-b hover:bg-gray-200">
                 <td
                   class="max-w-[250px] px-2 border border-r-0 text-ellipsis overflow-hidden hover:whitespace-normal hover:min-w-[200px]"
-                  v-for="key in keys"
-                  @mouseover="row.hover = row.hover || {}; row.hover[key] = true"
-                  @mouseleave="row.hover[key] = false"
-                >
+                  v-for="key in keys" @mouseover="row.hover = row.hover || {}; row.hover[key] = true"
+                  @mouseleave="row.hover[key] = false">
                   {{ row.metric[key] }}
                   <span class="inline-flex" v-if="row.hover && row.hover[key]">
-                    <button
-                      class="rounded px-1 border bg-slate-50 ml-1"
-                      @click="addLabel('', key, row.metric[key])"
-                    >
+                    <button class="rounded px-1 border bg-slate-50 ml-1" @click="addLabel('', key, row.metric[key])">
                       <i class="mdi mdi-plus-circle-outline"></i>
                     </button>
-                    <button
-                      class="rounded px-1 border bg-slate-50"
-                      @click="addLabel('!', key, row.metric[key])"
-                    >
+                    <button class="rounded px-1 border bg-slate-50" @click="addLabel('!', key, row.metric[key])">
                       <i class="mdi mdi-minus-circle-outline"></i>
                     </button>
                   </span>
@@ -122,49 +88,32 @@ import RunButton from "@/components/RunButton.vue"
       <div class="fixed right-0 bottom-0 bg-slate-300 text-xs pt-4 w-80">
         <div>
           <ul class="w-full flex list-none">
-            <li
-              class="py-3 basis-1/2 text-center hover:bg-slate-50 cursor-pointer border-b-2 border-transparent"
-              :class="tab == 0 ? 'active' : ''"
-              @click="tab = 0"
-            >Metrics ({{ Object.keys(metadata).length }})</li>
-            <li
-              class="py-3 basis-1/2 text-center hover:bg-slate-50 cursor-pointer border-b-2 border-transparent"
-              :class="tab == 1 ? 'active' : ''"
-              @click="tab = 1"
-            >Labels ({{ keys.length }})</li>
+            <li class="py-3 basis-1/2 text-center hover:bg-slate-50 cursor-pointer border-b-2 border-transparent"
+              :class="tab == 0 ? 'active' : ''" @click="tab = 0">Metrics ({{ Object.keys(metadata).length }})</li>
+            <li class="py-3 basis-1/2 text-center hover:bg-slate-50 cursor-pointer border-b-2 border-transparent"
+              :class="tab == 1 ? 'active' : ''" @click="tab = 1">Labels ({{ keys.length }})</li>
           </ul>
         </div>
         <div
           class="overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-track-rounded scrollbar-thumb-slate-300 scrollbar-track-transparnt dark:scrollbar-thumb-slate-900 border-l border-2 border-slate-300 w-full bg-slate-200 border-b"
-          style="height: calc(100vh - 100px);"
-        >
+          style="height: calc(100vh - 100px);">
           <div v-if="tab == 0">
             <div class v-for="(d, k) in metaDict">
-              <div
-                class="pl-1 cursor-pointer text-stone-600"
-                @click="d.showMetrics = !d.showMetrics"
-              >{{ k }} ({{ d.metrics.length }})</div>
-              <div
-                class="pl-4 overflow-hidden text-ellipsis hover:bg-white cursor-pointer"
-                v-if="d.showMetrics"
-                v-for="m in d.metrics"
-                @click="applyMetric(m)"
-                @mouseover="selectMetric(m)"
-              >{{ m.name }}</div>
+              <div class="pl-1 cursor-pointer text-stone-600" @click="d.showMetrics = !d.showMetrics">{{ k }} ({{
+                  d.metrics.length
+              }})</div>
+              <div class="pl-4 overflow-hidden text-ellipsis hover:bg-white cursor-pointer" v-if="d.showMetrics"
+                v-for="m in d.metrics" @click="applyMetric(m)" @mouseover="selectMetric(m)">{{ m.name }}</div>
             </div>
           </div>
           <div v-else>
             <div v-for="(d, key) in keyDict">
-              <div
-                class="pl-1 overflow-hidden text-ellipsis hover:bg-white cursor-pointer"
-                @click="d.show = !d.show"
-              >{{ key }} ({{ d.values.length }})</div>
+              <div class="pl-1 overflow-hidden text-ellipsis hover:bg-white cursor-pointer" @click="d.show = !d.show">{{
+                  key
+              }} ({{ d.values.length }})</div>
               <div class="pl-4" v-if="d.show" v-for="v in d.values">
                 {{ v }}
-                <button
-                  class="rounded px-1 border bg-slate-50 ml-1"
-                  @click="addLabel('', key, v)"
-                >
+                <button class="rounded px-1 border bg-slate-50 ml-1" @click="addLabel('', key, v)">
                   <i class="mdi mdi-plus-circle-outline"></i>
                 </button>
                 <button class="rounded px-1 border bg-slate-50" @click="addLabel('!', key, v)">
@@ -177,11 +126,8 @@ import RunButton from "@/components/RunButton.vue"
       </div>
     </div>
   </div>
-  <div
-    v-if="metricInfo"
-    v-click-outside="clickOutside"
-    class="fixed z-50 top-[9rem] right-[20.5rem] w-80 bg-white border border-slate-300 rounded opacity-[.9] hover:opacity-100"
-  >
+  <div v-if="metricInfo" v-click-outside="clickOutside"
+    class="fixed z-50 top-[9rem] right-[20.5rem] w-80 bg-white border border-slate-300 rounded opacity-[.9] hover:opacity-100">
     <div class="border-b border-slate-300 p-2 break-all font-bold">{{ metricInfo.name }}</div>
     <div class="px-2 py-1 word-break" v-for="(v, k) in metricInfo.data[0]">{{ v }}</div>
   </div>
@@ -199,6 +145,8 @@ export default {
   },
   computed: {
     items() {
+      console.log('items')
+      console.log('this.expr=', this.expr)
       const keyword = this.expr
       if (!keyword || keyword.length < 1) return []
       return Object.entries(this.metadata).filter(x => x[0].indexOf(keyword) >= 0).map(x => {
@@ -284,6 +232,8 @@ export default {
       this.searchMode = true
     },
     addLabel(not, key, value) {
+      console.log('addLabel')
+      console.log('this.expr=', this.expr)
       const where = `${key}${not}="${value}"`
       const idx = this.expr.indexOf('}')
       if (idx < 0) {
@@ -300,6 +250,8 @@ export default {
       this.range = r
     },
     async execute() {
+      console.log('execute')
+      console.log('this.expr=', this.expr)
       if (this.expr.length < 1) {
         console.error('emtpy expr')
         return
@@ -393,6 +345,8 @@ export default {
       this.metricInfo = m
     },
     applyMetric(m) {
+      console.log('applyMetric')
+      console.log('this.expr=', this.expr)
       this.metricInfo.selected = null
       this.expr = m.name
     },
@@ -430,10 +384,13 @@ export default {
     },
   },
   mounted() {
+    console.log('mounted')
+    console.log('this.expr=', this.expr)
+      
     useTimeStore().timerManager = 'MetricsView'
     this.fetchMetadata()
-    if(this.$route.query?.query) {
-      this.expr = ''+ this.$route.query.query
+    if (this.$route.query?.query) {
+      this.expr = '' + this.$route.query.query
       setTimeout(this.execute, 500)
     }
     window.addEventListener("resize", this.chartResize)
@@ -448,6 +405,7 @@ export default {
 .headcol-before:before {
   content: "Row ";
 }
+
 .u-inline.u-live th::after {
   content: "";
 }
@@ -481,11 +439,11 @@ export default {
   @apply font-medium text-left;
 }
 
-.u-legend th > .u-marker {
+.u-legend th>.u-marker {
   @apply w-3 h-3;
 }
 
-.u-legend th > .u-label {
+.u-legend th>.u-label {
   @apply inline;
 }
 
