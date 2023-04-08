397292737581cac3627bc9105b6438f069e0163c
sungwoojang <sw0216.jang@samsung.com>
2023-04-08 18:02:40 +0900

    add k8s clientset test part and discovery with annotation, without annotation case

--- pkg/store/datasource_store_test.go
+++ pkg/store/datasource_store_test.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
 	"testing"
 	"time"
 
@@ -10,7 +12,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func makeService(name string, namespace string, multiport bool) v1.Service {
+func makeService(name string, namespace string, multiport bool, annotation map[string]string) runtime.Object {
 	ports := []v1.ServicePort{
 		{
 			Name:     "testport",
@@ -25,17 +27,19 @@ func makeService(name string, namespace string, multiport bool) v1.Service {
 			Port:     int32(8080),
 		})
 	}
-	return v1.Service{
+
+	return runtime.Object(&v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
+			Name:        name,
+			Namespace:   namespace,
+			Annotations: annotation,
 		},
 		Spec: v1.ServiceSpec{
 			Ports:     ports,
 			Type:      v1.ServiceTypeClusterIP,
 			ClusterIP: "10.0.0.1",
 		},
-	}
+	})
 }
 
 func TestNewDatasourceStore(t *testing.T) {
@@ -62,7 +66,15 @@ func TestNewDatasourceStore(t *testing.T) {
 	assert.ElementsMatch(t, store.datasources, datasources)
 }
 
-func TestGetDatasourcesFromServices(t *testing.T) {
+var servicesWithoutAnnotation = []runtime.Object{
+	makeService("prometheus", "namespace1", false, nil),
+	makeService("prometheus", "namespace2", false, nil),
+	makeService("prometheus", "kube-system", false, nil),
+	makeService("lethe", "kuoss", true, nil),
+	makeService("lethe", "kube-system", true, nil),
+}
+
+func TestDiscoverDatasourcesByName(t *testing.T) {
 	datasourcesConfig := &configuration.DatasourcesConfig{
 		QueryTimeout: time.Second * 10,
 		Datasources:  []*configuration.Datasource{},
@@ -72,20 +84,119 @@ func TestGetDatasourcesFromServices(t *testing.T) {
 			ByNameLethe:      true,
 		},
 	}
+	want := []configuration.Datasource{
+		{
+			Type:         "lethe",
+			Name:         "lethe.kube-system",
+			URL:          "http://lethe.kube-system:8080",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.kube-system",
+			URL:          "http://prometheus.kube-system:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "lethe",
+			Name:         "lethe.kuoss",
+			URL:          "http://lethe.kuoss:8080",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.namespace1",
+			URL:          "http://prometheus.namespace1:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.namespace2",
+			URL:          "http://prometheus.namespace2:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		}}
 	store, _ := NewDatasourceStore(datasourcesConfig)
-	services := []v1.Service{
-		makeService("prometheus", "namespace1", false),
-		makeService("prometheus", "namespace2", false),
-		makeService("prometheus", "kube-system", false),
-		makeService("lethe", "kuoss", true),
-		makeService("lethe", "kube-system", true),
+	got, err := store.discoverDatasources(fake.NewSimpleClientset(servicesWithoutAnnotation...))
+	if err != nil {
+		return
 	}
-	got := store.getDatasourcesFromServices(services)
-	assert.ElementsMatch(t, []configuration.Datasource{
-		{Type: "prometheus", Name: "prometheus.namespace1", URL: "http://prometheus.namespace1:30900", BasicAuth: false, BasicAuthUser: "", BasicAuthPassword: "", IsMain: false, IsDiscovered: true},
-		{Type: "prometheus", Name: "prometheus.namespace2", URL: "http://prometheus.namespace2:30900", BasicAuth: false, BasicAuthUser: "", BasicAuthPassword: "", IsMain: false, IsDiscovered: true},
-		{Type: "prometheus", Name: "prometheus.kube-system", URL: "http://prometheus.kube-system:30900", BasicAuth: false, BasicAuthUser: "", BasicAuthPassword: "", IsMain: false, IsDiscovered: true},
-		{Type: "lethe", Name: "lethe.kuoss", URL: "http://lethe.kuoss:8080", BasicAuth: false, BasicAuthUser: "", BasicAuthPassword: "", IsMain: false, IsDiscovered: true},
-		{Type: "lethe", Name: "lethe.kube-system", URL: "http://lethe.kube-system:8080", BasicAuth: false, BasicAuthUser: "", BasicAuthPassword: "", IsMain: false, IsDiscovered: true},
-	}, got)
+	assert.ElementsMatch(t, want, got)
+}
+
+var servicesWithAnnotation = []runtime.Object{
+	makeService("prometheus", "namespace1", false, map[string]string{
+		"kuoss.org/datasource-type": "prometheus",
+	}),
+	makeService("prometheus", "namespace2", false, map[string]string{
+		"kuoss.org/datasource-type": "prometheus",
+	}),
+	makeService("prometheus", "kube-system", false, map[string]string{
+		"kuoss.org/datasource-type": "prometheus",
+	}),
+	makeService("lethe", "kuoss", true, map[string]string{
+		"kuoss.org/datasource-type": "lethe",
+	}),
+	makeService("lethe", "kube-system", true, map[string]string{
+		"kuoss.org/datasource-type": "lethe",
+	}),
+}
+
+func TestDiscoverDatasourcesByAnnotationKey(t *testing.T) {
+	datasourcesConfig := &configuration.DatasourcesConfig{
+		QueryTimeout: time.Second * 10,
+		Datasources:  []*configuration.Datasource{},
+		Discovery: configuration.Discovery{
+			Enabled:          false, // cheat
+			AnnotationKey:    "kuoss.org/datasource-type",
+			ByNamePrometheus: false,
+			ByNameLethe:      false,
+		},
+	}
+	want := []configuration.Datasource{
+		{
+			Type:         "lethe",
+			Name:         "lethe.kube-system",
+			URL:          "http://lethe.kube-system:8080",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.kube-system",
+			URL:          "http://prometheus.kube-system:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "lethe",
+			Name:         "lethe.kuoss",
+			URL:          "http://lethe.kuoss:8080",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.namespace1",
+			URL:          "http://prometheus.namespace1:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		},
+		{
+			Type:         "prometheus",
+			Name:         "prometheus.namespace2",
+			URL:          "http://prometheus.namespace2:30900",
+			IsMain:       false,
+			IsDiscovered: true,
+		}}
+	store, _ := NewDatasourceStore(datasourcesConfig)
+	got, err := store.discoverDatasources(fake.NewSimpleClientset(servicesWithAnnotation...))
+	if err != nil {
+		return
+	}
+	assert.ElementsMatch(t, got, want)
 }
