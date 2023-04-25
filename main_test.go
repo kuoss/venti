4868d5c997f425fc62e47aa19ccf68c4eb2da9e9
jmnote <opcore@gmail.com>
2023-04-25 15:59:06 +0000

    minor edit

--- main_test.go
+++ main_test.go
@@ -14,8 +14,8 @@ func Test_main(t *testing.T) {
 	stdout, stderr, err := tester.CaptureChildTest(func() {
 		main()
 	})
-	assert.Equal(t, "", stdout)
-	assert.Contains(t, stderr, "config load failed: error on loadDatasourceConfigFromFilepath: error on ReadFile: open etc/datasources.yaml: no such file or directory")
 	assert.Error(t, err)
 	assert.EqualError(t, err, "exit status 1")
+	assert.Equal(t, "", stdout)
+	assert.Contains(t, stderr, "config load failed: error on loadDatasourceConfigFromFilepath: error on ReadFile: open etc/datasources.yaml: no such file or directory")
 }
