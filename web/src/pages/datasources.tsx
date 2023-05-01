2900638dfc598b77384334255ba79103d233a444
jmnote <opcore@gmail.com>
2023-05-01 10:47:43 +0000

    return to vue

--- web/src/pages/datasources.tsx
+++
@@ -1,3 +0,0 @@
-export default function Datasources() {
-  return <div>Datasources</div>
-}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
+  plugins: [require('nightwind'), require('tailwind-scrollbar')],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // animation: {
+      //   'reverse-spin': 'reverse-spin 3s linear infinite'
+      // },
+      // keyframes: {
+      //   'reverse-spin': {
+      //     from: {
+      //       transform: 'rotate(360deg)'
+      //     },
+      //   }
+      // }
     },
   },
-  plugins: [require("nightwind")],
 }
