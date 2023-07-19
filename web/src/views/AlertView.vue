c68d03f8381720b9f3842e817af6763944c1a145
jmnote <opcore@gmail.com>
2023-07-19 02:21:36 +0000

    unident

--- web/src/views/AlertView.vue
+++ web/src/views/AlertView.vue
@@ -1,133 +1,132 @@
 <script setup>
-  import Util from '@/lib/util';
-  </script>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        alertFiles: [],
-        isLoading: false,
-        repeat: true,
-        testAlertSent: false,
-      };
-    },
-    mounted() {
-      this.fetchData();
-    },
-    beforeUnmount() {
-      this.repeat = false;
-    },
-    methods: {
-      async fetchData() {
-        this.isLoading = true;
-        try {
-          const response = await fetch('/api/v1/alerts');
-          const jsonData = await response.json();
-          this.alertFiles = jsonData;
-          setTimeout(() => {
-            if (!this.repeat) return;
-            this.fetchData();
-          }, 3000);
-        } catch (error) {
-          this.repeat = false;
-          console.error(error);
-        }
-        this.isLoading = false;
-      },
-      sendTestAlert() {
-        try {
-          const response = await fetch('/api/v1/alerts/test');
-          const jsonData = await response.json();
-          console.log(jsonData)
-        } catch (error) {
-          console.error(error);
-        }
-        this.testAlertSent = true;
+import Util from '@/lib/util';
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      alertFiles: [],
+      isLoading: false,
+      repeat: true,
+      testAlertSent: false,
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  beforeUnmount() {
+    this.repeat = false;
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      try {
+        const response = await fetch('/api/v1/alerts');
+        const jsonData = await response.json();
+        this.alertFiles = jsonData;
+        setTimeout(() => {
+          if (!this.repeat) return;
+          this.fetchData();
+        }, 3000);
+      } catch (error) {
+        this.repeat = false;
+        console.error(error);
       }
+      this.isLoading = false;
     },
-  };
-  </script>
-  
-  <template>
-    <div>
-      <header class="fixed right-0 w-full bg-white border-b shadow z-30 p-2 pl-52" :class="{ 'is-loading': isLoading }">
-        <div class="flex items-center flex-row">
-          <div><i class="mdi mdi-18px mdi-database-outline" /> Alert</div>
-          <div class="flex ml-auto">
-            <div class="inline-flex">
-              <button class="h-rounded-group py-2 px-4 text-gray-900 bg-white border border-common" v-if="!testAlertSent" @click="sendTestAlert">
-                <i class="mdi mdi-cube-send" /> Send Test Alert
-              </button>
-              <button class="h-rounded-group py-2 px-4 text-gray-900 bg-white border border-common">
-                <i class="mdi mdi-refresh mdi-spin" />
-              </button>
-            </div>
+    sendTestAlert() {
+      try {
+        const response = await fetch('/api/v1/alerts/test');
+        const jsonData = await response.json();
+        console.log(jsonData)
+      } catch (error) {
+        console.error(error);
+      }
+      this.testAlertSent = true;
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <header class="fixed right-0 w-full bg-white border-b shadow z-30 p-2 pl-52" :class="{ 'is-loading': isLoading }">
+      <div class="flex items-center flex-row">
+        <div><i class="mdi mdi-18px mdi-database-outline" /> Alert</div>
+        <div class="flex ml-auto">
+          <div class="inline-flex">
+            <button class="h-rounded-group py-2 px-4 text-gray-900 bg-white border border-common" v-if="!testAlertSent" @click="sendTestAlert">
+              <i class="mdi mdi-cube-send" /> Send Test Alert
+            </button>
+            <button class="h-rounded-group py-2 px-4 text-gray-900 bg-white border border-common">
+              <i class="mdi mdi-refresh mdi-spin" />
+            </button>
           </div>
         </div>
-      </header>
-  
-      <main class="mt-12 w-full p-8 pb-16">
-        <h1 class="py-2 font-bold">Alert Rule Files ({{ alertFiles.length }} files)</h1>
-        <table class="w-full bg-white border">
-          <tr class="border-b bg-slate-50">
-            <th>State</th>
-            <th>Severity</th>
-            <th>Name</th>
-            <th>Summary</th>
-            <th>Expr</th>
-            <th>For</th>
+      </div>
+    </header>
+
+    <main class="mt-12 w-full p-8 pb-16">
+      <h1 class="py-2 font-bold">Alert Rule Files ({{ alertFiles.length }} files)</h1>
+      <table class="w-full bg-white border">
+        <tr class="border-b bg-slate-50">
+          <th>State</th>
+          <th>Severity</th>
+          <th>Name</th>
+          <th>Summary</th>
+          <th>Expr</th>
+          <th>For</th>
+        </tr>
+        <tbody v-for="f in alertFiles">
+          <tr class="border-b">
+            <th class="bg-slate-300 p-1 pl-3" colspan="6">
+              {{ f.datasourceSelector.type == 'prometheus' ? '🔥' : '💧' }} file ({{ f.groups.length }} groups)
+            </th>
           </tr>
-          <tbody v-for="f in alertFiles">
+          <template v-for="g in f.groups">
             <tr class="border-b">
-              <th class="bg-slate-300 p-1 pl-3" colspan="6">
-                {{ f.datasourceSelector.type == 'prometheus' ? '🔥' : '💧' }} file ({{ f.groups.length }} groups)
-              </th>
+              <th class="bg-slate-200 p-1 pl-6" colspan="6">group: {{ g.name }} ({{ g.ruleAlerts.length }} rules)</th>
             </tr>
-            <template v-for="g in f.groups">
-              <tr class="border-b">
-                <th class="bg-slate-200 p-1 pl-6" colspan="6">group: {{ g.name }} ({{ g.ruleAlerts.length }} rules)</th>
-              </tr>
-              <tr v-for="ra in g.ruleAlerts" class="border-b">
-                <td class="p-1 pl-9">
-                  <span v-for="a in ra.alerts">
-                    <div v-if="a.state == 'firing'" class="rounded w-24 text-center bg-red-300">firing</div>
-                    <div v-else-if="a.state == 'pending'" class="rounded w-24 text-center bg-yellow-300">pending</div>
-                    <div v-else class="rounded w-24 text-center bg-green-300">normal</div>
-                  </span>
-                </td>
-                <td class="px-2">
-                  {{ f.commonLabels?.severity }}
-                </td>
-                <td class="px-2">
-                  {{ ra.rule.alert }}
-                </td>
-                <td class="px-2 max-w-[30vw] truncate hover:whitespace-normal">
-                  {{ ra.rule.annotations?.summary }}
-                </td>
-                <td class="px-2 max-w-[20vw] truncate hover:whitespace-normal text-cyan-500">
-                  <a
-                    class="hover:underline"
-                    :href="`/${f.datasourceSelector.type == 'prometheus' ? 'metrics' : 'logs'}?query=${encodeURIComponent(
-                      ra.rule.expr,
-                    )}`"
-                    >{{ ra.rule.expr }}</a
-                  >
-                </td>
-                <td class="px-2">
-                  {{ Util.nanoseconds2human(ra.rule.for) }}
-                </td>
-              </tr>
-            </template>
-          </tbody>
-        </table>
-      </main>
-    </div>
-  </template>
-  
-  <style>
-  th {
-    @apply text-left px-2;
-  }
-  </style>
-  
+            <tr v-for="ra in g.ruleAlerts" class="border-b">
+              <td class="p-1 pl-9">
+                <span v-for="a in ra.alerts">
+                  <div v-if="a.state == 'firing'" class="rounded w-24 text-center bg-red-300">firing</div>
+                  <div v-else-if="a.state == 'pending'" class="rounded w-24 text-center bg-yellow-300">pending</div>
+                  <div v-else class="rounded w-24 text-center bg-green-300">normal</div>
+                </span>
+              </td>
+              <td class="px-2">
+                {{ f.commonLabels?.severity }}
+              </td>
+              <td class="px-2">
+                {{ ra.rule.alert }}
+              </td>
+              <td class="px-2 max-w-[30vw] truncate hover:whitespace-normal">
+                {{ ra.rule.annotations?.summary }}
+              </td>
+              <td class="px-2 max-w-[20vw] truncate hover:whitespace-normal text-cyan-500">
+                <a
+                  class="hover:underline"
+                  :href="`/${f.datasourceSelector.type == 'prometheus' ? 'metrics' : 'logs'}?query=${encodeURIComponent(
+                    ra.rule.expr,
+                  )}`"
+                  >{{ ra.rule.expr }}</a
+                >
+              </td>
+              <td class="px-2">
+                {{ Util.nanoseconds2human(ra.rule.for) }}
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </main>
+  </div>
+</template>
+
+<style>
+th {
+  @apply text-left px-2;
+}
+</style>
