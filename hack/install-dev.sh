031a469deeb43013aa9dd3986312a383517b0dd8
jmnote <opcore@gmail.com>
2024-04-04 09:50:03 +0000

    dark PanelLogs + a

---
+++ hack/install-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+which npm
+retVal=$?
+if [[ $retVal -ne 0 ]]; then
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    [ -f /etc/apt/keyrings/nodesource.gpg ] || curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
+    sudo apt-get install nodejs -y
+fi
+go mod tidy
+which air   || go install github.com/cosmtrek/air@latest
+which godef || go install github.com/rogpeppe/godef@latest
+cd web && npm install
